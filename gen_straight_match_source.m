df951325d2a619296c10239822ce6a3de6dce96d
money999 <foney939@gmail.com>
2015-11-19 21:21:27 +0800

    更改直接数据

--- gen_straight_match_source.m
+++ gen_straight_match_source.m
@@ -1,13 +1,21 @@
-function [ position, velocity, class, posG1, velG1, claG1, posG2, velG2, claG2] = gen_straight_match_source()
+function [adis] = gen_straight_match_source()
 %UNTITLED2 此处显示有关此函数的摘要
-%   num         生成多少个点
-%   square      点在平面内的范围
-%   v_dir       速度的方向取值范围
-%   v_nor       速度的大小取值范围
-%   c_num       类别个数
 %   此处显示详细说明
 
+pp = [0 1 2 3 4 5 6 8];  %8
+vv = [0 0.3 0.7 1 1.8 2.4];%6
+cc = [0 0.3 0.4 0.5 0.4 0.7 1];%7
 
+adis = zeros(336,3);
+ntt = 1;
+for i = 1:8
+    for j = 1:6
+        for k = 1:7
+            adis(ntt, :) = [pp(i) vv(j) cc(k)];
+            ntt = ntt + 1;
+        end
+    end
+end
 
 
 end
